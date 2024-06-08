253af7702975792394f453ff700541b4fde28705
shiv-ko <iscopre7@gmail.com>
2024-06-09 04:09:53 +0900

    push

--- src/components/memories.tsx
+++ src/components/memories.tsx
@@ -1,18 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  doc,
-  getDoc,
-} from "firebase/firestore";
-import {
-  getStorage,
-  ref as storageRef,
-  getDownloadURL,
-  uploadString,
-} from "firebase/storage";
+import { getFirestore, collection, getDocs, doc, getDoc } from "firebase/firestore";
+import { getStorage, ref as storageRef, getDownloadURL, uploadString } from "firebase/storage";
 import Unity, { UnityContext } from "react-unity-webgl";
 import html2canvas from "html2canvas";
 import Poteto from "./poteto_icon.png";
@@ -37,13 +26,18 @@ const Memories: React.FC = () => {
   const [currentYear, setCurrentYear] = useState(today.getFullYear());
   const [selectedDate, setSelectedDate] = useState<string | null>(null);
   const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
-  const [unityInstanceUrl, setUnityInstanceUrl] =
-    useState<UnityInstanceUrls | null>(null);
+  const [unityInstanceUrl, setUnityInstanceUrl] = useState<UnityInstanceUrls | null>({
+    loaderUrl: "/unity/test15.loader.js",
+    dataUrl: "/unity/test15.data",
+    frameworkUrl: "/unity/test15.framework.js",
+    codeUrl: "/unity/test15.wasm",
+  });
   const [user, setUser] = useState<any>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [events, setEvents] = useState<string[]>([]);
   const [burgerConfig, setBurgerConfig] = useState<BurgerConfig | null>(null);
+  const [showSuccessPopup, setShowSuccessPopup] = useState(false);
   const unityRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -61,12 +55,7 @@ const Memories: React.FC = () => {
       if (currentUser) {
         setUser(currentUser);
         try {
-          const userEventsRef = collection(
-            db,
-            "Users_Burger",
-            currentUser.uid,
-            "BurgerData"
-          );
+          const userEventsRef = collection(db, "Users_Burger", currentUser.uid, "BurgerData");
           const snapshot = await getDocs(userEventsRef);
           const datesWithEvents = snapshot.docs.map((doc) => doc.id);
           setEvents(datesWithEvents);
@@ -96,35 +85,12 @@ const Memories: React.FC = () => {
     const currentUser = getAuth().currentUser;
 
     if (currentUser) {
-      const docRef = doc(
-        db,
-        "Users_Burger",
-        currentUser.uid,
-        "BurgerData",
-        yymmdd
-      );
+      const docRef = doc(db, "Users_Burger", currentUser.uid, "BurgerData", yymmdd);
       const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
         const configData = docSnap.data() as BurgerConfig;
         setBurgerConfig(configData);
-
-        const storage = getStorage();
-        const files = await Promise.all([
-          getDownloadURL(storageRef(storage, `Burger_webgl/test15.data`)),
-          getDownloadURL(
-            storageRef(storage, `Burger_webgl/test15.framework.js`)
-          ),
-          getDownloadURL(storageRef(storage, `Burger_webgl/test15.wasm`)),
-          getDownloadURL(storageRef(storage, `Burger_webgl/test15.loader.js`)),
-        ]);
-        setUnityInstanceUrl({
-          dataUrl: files[0],
-          frameworkUrl: files[1],
-          codeUrl: files[2],
-          loaderUrl: files[3],
-        });
       } else {
-        setUnityInstanceUrl(null);
         setBurgerConfig(null);
       }
     }
@@ -146,7 +112,17 @@ const Memories: React.FC = () => {
 
     try {
       const canvas = await html2canvas(unityRef.current, { useCORS: true });
-      const imageData = canvas.toDataURL("image/png");
+      const squareCanvas = document.createElement('canvas');
+      const squareSize = Math.max(canvas.width, canvas.height);
+      squareCanvas.width = squareSize;
+      squareCanvas.height = squareSize;
+      const context = squareCanvas.getContext('2d');
+      if (context) {
+        context.fillStyle = '#ffffff'; // Set the background color to white
+        context.fillRect(0, 0, squareSize, squareSize);
+        context.drawImage(canvas, (squareSize - canvas.width) / 2, (squareSize - canvas.height) / 2);
+      }
+      const imageData = squareCanvas.toDataURL("image/png");
 
       const storage = getStorage();
       const storagePath = `User_Collection/${user.uid}/${selectedDate}.png`;
@@ -154,6 +130,8 @@ const Memories: React.FC = () => {
 
       await uploadString(imageRef, imageData, "data_url");
       console.log("Screenshot saved successfully");
+      setShowSuccessPopup(true);
+      setTimeout(() => setShowSuccessPopup(false), 3000);
     } catch (error) {
       console.error("Error saving screenshot: ", error);
     }
@@ -173,54 +151,27 @@ const Memories: React.FC = () => {
     useEffect(() => {
       if (burgerConfig) {
         unityContext.on("loaded", () => {
-          unityContext.send(
-            "Scripts",
-            "ConfigureBurger",
-            JSON.stringify(burgerConfig)
-          );
+          unityContext.send("Scripts", "ConfigureBurger", JSON.stringify(burgerConfig));
         });
       }
     }, [unityContext, burgerConfig]);
 
-    return (
-      <Unity
-        unityContext={unityContext}
-        style={{ width: "95%", height: `${viewportHeight - 450}px` }}
-      />
-    );
+    return <Unity unityContext={unityContext} style={{ width: "95%", height: `${viewportHeight - 450}px` }} />;
   };
 
   const daysInMonth = getDaysInMonth(currentYear, currentMonth);
   const formattedToday = formatDate(today);
 
   return (
-    <div
-      className="w-full flex flex-col items-start justify-start"
-      style={{
-        height: `${viewportHeight - 120}px`,
-        backgroundColor: "#F9ECCB",
-      }}
-    >
+    <div className="w-full flex flex-col items-start justify-start" style={{ height: `${viewportHeight - 120}px`, backgroundColor: "#F9ECCB" }}>
+      {showSuccessPopup && <div className="popup">Screenshot saved successfully!</div>}
       <div className="header w-full shadow-md rounded-lg overflow-hidden bg-white">
-        <div
-          className="flex items-center justify-between p-4"
-          style={{ backgroundColor: "#1a237e" }}
-        >
-          <button
-            className="text-gray-500"
-            onClick={() =>
-              setCurrentMonth((prev) => (prev === 0 ? 11 : prev - 1))
-            }
-          >
+        <div className="flex items-center justify-between p-4" style={{ backgroundColor: "#1a237e" }}>
+          <button className="text-gray-500" onClick={() => setCurrentMonth((prev) => (prev === 0 ? 11 : prev - 1))}>
             &lt;
           </button>
           <h2 className="text-lg font-bold text-white cursor-pointer">{`${currentYear}年${currentMonth + 1}月`}</h2>
-          <button
-            className="text-gray-500"
-            onClick={() =>
-              setCurrentMonth((prev) => (prev === 11 ? 0 : prev + 1))
-            }
-          >
+          <button className="text-gray-500" onClick={() => setCurrentMonth((prev) => (prev === 11 ? 0 : prev + 1))}>
             &gt;
           </button>
         </div>
@@ -232,21 +183,14 @@ const Memories: React.FC = () => {
           ))}
         </div>
         <div className="grid grid-cols-7 text-center bg-white">
-          {Array.from({
-            length: new Date(currentYear, currentMonth, 1).getDay(),
-          }).map((_, index) => (
+          {Array.from({ length: new Date(currentYear, currentMonth, 1).getDay() }).map((_, index) => (
             <div key={index} className="py-2"></div>
           ))}
           {Array.from({ length: daysInMonth }).map((_, dateIndex) => {
             const date = dateIndex + 1;
-            const yymmdd = formatDate(
-              new Date(currentYear, currentMonth, date)
-            );
+            const yymmdd = formatDate(new Date(currentYear, currentMonth, date));
             const hasEvent = events.includes(yymmdd);
-            const isToday =
-              date === today.getDate() &&
-              currentMonth === today.getMonth() &&
-              currentYear === today.getFullYear();
+            const isToday = date === today.getDate() && currentMonth === today.getMonth() && currentYear === today.getFullYear();
             return (
               <div
                 key={dateIndex}
@@ -266,10 +210,7 @@ const Memories: React.FC = () => {
           <div ref={unityRef} style={{ width: "95%" }}>
             <UnityInstance files={unityInstanceUrl} />
           </div>
-          <button
-            onClick={saveScreenshot}
-            style={{ marginLeft: "10px", alignSelf: "center" }}
-          >
+          <button onClick={saveScreenshot} style={{ marginLeft: "10px", alignSelf: "center" }}>
             <img src={Poteto} alt="Save Screenshot" />
           </button>
         </div>
@@ -293,6 +234,17 @@ const Memories: React.FC = () => {
         }
         .day.today {
           border: 2px solid blue;
+        }
+        .popup {
+          position: fixed;
+          top: 20px;
+          left: 50%;
+          transform: translateX(-50%);
+          background-color: #4caf50;
+          color: white;
+          padding: 10px 20px;
+          border-radius: 5px;
+          box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
         }
       `}</style>
     </div>
